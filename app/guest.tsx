50335f2fad7c42e9bdde91d8b71d959232dbe77e
Tianyu Liu <tyliu1122@gmail.com>
2025-11-16 16:41:00 +1100

    Add about and contact

---
+++ app/guest.tsx
@@ -0,0 +1,8 @@
+export default function Guest() {
+  return (
+    <div>
+      <h1>Welcome Guest</h1>
+      <p>Please sign in to continue</p>
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,14 @@
-export default function HomePage() {
+import { currentUser } from "@clerk/nextjs/server"
+import Guest from './guest'
+
+export default async function HomePage() {
+
+  const user = await currentUser();
+  
+  if (!user) {
+    return <Guest />
+  }
+
   return (
     <div className='text-red-400'> Home Page</div>
   )
